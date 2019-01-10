2251f7bfd04f915dd12489232c4d3e2f932d7857
Eddie Hunsinger <43016799+edyhsgr@users.noreply.github.com>
2019-01-10 00:47:55 -0900

    Update and rename getUsaMortalityData-v0.1.R to getUsaMortalityData-v0.1_WithDeathsDistributionGraphing.R

--- getUsaMortalityData-v0.1_WithDeathsDistributionGraphing.R
+++ getUsaMortalityData-v0.1_WithDeathsDistributionGraphing.R
@@ -1,7 +1,9 @@
 ##########
 ##########
-##Testing GitHub Fork of tool to download and import data for ndx graphing: 
-##  Sam Clark's getUsaMortalityData-v0.1.R (https://github.com/sinafala/usa-mortality/blob/master/getUsaMortalityData-v0.1.R)
+##Trying GitHub Fork of tool to download and import data for ndx graphing: 
+##Modified Sam Clark's getUsaMortalityData-v0.1.R 
+##(https://github.com/sinafala/usa-mortality/blob/master/getUsaMortalityData-v0.1.R)
+##With graphing steps added at the end. 
 ##
 setwd("")
 ##########
